29f7d5ebd53ccd11d402c7830050775b7b61a78e
juicybob123 <okruh2028@kehillahstudent.org>
2026-02-12 01:27:11 -0800

    oo

---
+++ q3.py
@@ -0,0 +1,14 @@
+
+
+def average(numbers):
+    x = 0
+    y= 0
+    for index in numbers:
+        x = x +index
+        y = y + 1
+      
+    print(x/y)
+average([32,78,48,71,93,44,5,42])
+average([1,2,3,4,5])
+
+
